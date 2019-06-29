26dac3bd9936e648f1d82003a4e72e29b2da1b90
Joseph Mtinangi <josephmtinangi@gmail.com>
2019-06-29 16:33:28 +0300

    Setup dashboard and login uI

--- src/app/auth/logout/logout.component.ts
+++ src/app/auth/logout/logout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-logout',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.logout()
+  }
+
+  logout() {
+    this.authService.logout();
   }
 
 }
--- src/app/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { User } from 'src/app/_models/user.model';
 
 @Component({
   selector: 'app-dashboard-layout',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardLayoutComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.getUser();
   }
+
+  getUser():void {
+    this.user = JSON.parse(this.authService.getAuthenticatedUser());
+  }  
 
 }
